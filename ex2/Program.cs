fe26477f009f90d8eb59de1818ec1e30218b5770
Vadim <54818214+DylVadim@users.noreply.github.com>
2023-06-15 02:23:49 +0400

    ex2_done
    Задание выполнено и готово к проверке.

--- ex2/Program.cs
+++ ex2/Program.cs
@@ -5,3 +5,18 @@ a = 2 b = 10 -> max = 10
 a = -9 b = -3 -> max = -3
 */
 
+Console.Clear();
+Console.Write("Введите Первое число: ");
+string UserInputNumber_a = Console.ReadLine() ?? "";
+int a = int.Parse(UserInputNumber_a);
+Console.Write("Введите Второе число: ");
+string UserInputNumber_b = Console.ReadLine() ?? "";
+int b = int.Parse(UserInputNumber_b);
+int MaxNum = b;
+if(a > b)
+{
+    MaxNum = a;
+}
+
+
+Console.WriteLine($"Из чисел {a} и {b} Максимально число -> {MaxNum}");
