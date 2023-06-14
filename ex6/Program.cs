293b96b98aee44180fbff3c52009c2a44dd51f11
Vadim <54818214+DylVadim@users.noreply.github.com>
2023-06-15 03:07:27 +0400

    ex6 Done
    Задание 6 выполнено

--- ex6/Program.cs
+++ ex6/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+4 -> да
+-3 -> нет
+7 -> нет
+*/
+
+Console.Clear();
+Console.Write("Введите целое число: ");
+string UserInputNumber = Console.ReadLine() ?? "";
+int Num = int.Parse(UserInputNumber);
+
+if(Num % 2 == 0)
+{
+    Console.WriteLine($"Число {Num} является чётным");
+}
+else
+{
+    Console.WriteLine($"Число {Num} не является чётным");
+}
