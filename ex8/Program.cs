4377fff9ac9697fdcee337d6dffe0e40e2715f7f
Vadim <54818214+DylVadim@users.noreply.github.com>
2023-06-15 03:49:29 +0400

    ex8 Done
    Единственное я не смог вывести в строку значения, цикл создаёт каждую итерацию на новой строке.

--- ex8/Program.cs
+++ ex8/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+5 -> 2, 4
+8 -> 2, 4, 6, 8
+*/
+
+Console.Clear();
+Console.WriteLine("Программа показывает все чётные числа от 1 до ");
+Console.Write("Введите целое число: ");
+string UserInputNumber = Console.ReadLine() ?? "";
+int Num = int.Parse(UserInputNumber);
+int Count = 1;
+
+while (Count <= Num)
+{
+    if(Count % 2 == 0)
+    {
+        Console.WriteLine($"{Num} -> {Count},");
+        Count++;
+
+    }
+    else
+    {
+        Count++;
+    }
+}
