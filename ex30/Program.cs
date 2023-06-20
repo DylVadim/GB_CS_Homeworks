57772a1b564dd6de84f20d59942c6c3ee9224794
VadimDyl <Dyl.vadim.a@gmail.com>
2023-06-20 22:18:53 +0400

    решение задачи 30

---
+++ ex30/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Clear(); 
+int[] randomArray = new int[8]; 
+
+
+
+for (int i = 0; i < randomArray.Length; i++)
+{
+    randomArray[i] = Rand ();
+
+    Console.Write($"{randomArray[i]} "); 
+}
+
+int Rand()
+{
+    return new Random().Next(0,2); 
+}
