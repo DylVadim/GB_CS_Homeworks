26b67c2b2c0d12c75a99fbe03984a123bcad78c6
VadimDyl <Dyl.vadim.a@gmail.com>
2023-06-20 22:09:50 +0400

    решение задачи 28

--- ex28/Program.cs
+++ ex28/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+int number = GetNumberFromUser("Введите целое число N: ", "Ошибка ввода!");
+int prodNumbers = GetProdNumbers(number);
+Console.WriteLine($"{number} -> {prodNumbers}");
+
+
+
+int GetNumberFromUser(string message, string errorMessage)
+{
+    while(true)
+    {
+        Console.Write(message);
+        if(int.TryParse(Console.ReadLine(), out int userNumber))
+            return userNumber;
+        Console.WriteLine(errorMessage);
+    }
+}
+
+int GetProdNumbers(int number)
+{
+    int prod = 1;
+    while(number > 0)
+    {
+        prod *= number;
+        number--;
+    }
+    return prod;
+}
