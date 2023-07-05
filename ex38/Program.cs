82d8502c62f8dff1f2ad8dbceed6fe14af363f16
Vadim <dyl.vadim.a@gmail.com>
2023-07-06 01:51:47 +0400

    38 задача выполнено

---
+++ ex38/Program.cs
@@ -0,0 +1,49 @@
+﻿double[] array = GenerateDoubleArray(1,10,5);              
+                                                   
+PrintArray(array);                                
+
+GetDifference(array);
+
+/////////////////////////////////////////////////////////////
+
+
+double [] GenerateDoubleArray(int min, int max, int length)   
+{                                                   
+    Random random = new Random();                   
+    double[] array = new double [length];                  
+                                                    
+    for (var i = 0; i < array.Length; i++)          
+    {
+        array[i] = random.NextDouble();       
+    }
+    return array;                                  
+} 
+
+
+void PrintArray(double[] array)                       
+{
+     Console.WriteLine(@"[{0}]", string.Join("; ", array)); 
+}                                         
+
+
+void GetDifference(double [] array)   
+{                                                   
+    double min = array[0];
+    double max = array[0];
+    double diff = 0;
+                                                  
+    for (var i = 0; i < array.Length; i++)          
+    {
+       if (array[i] > max)
+          {
+            max = array[i];
+          }
+         
+            if (array[i] < min)
+           {
+                min = array[i];
+           }
+    }
+    diff = max - min ;
+    Console.WriteLine($" => {max} - {min} = {diff}.");
+}
