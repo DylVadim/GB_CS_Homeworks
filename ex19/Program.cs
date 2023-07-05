288da42bbdad296df003dd3d7508b9a6d2eeda67
Vadim <dyl.vadim.a@gmail.com>
2023-07-05 22:52:11 +0400

    19 задача выполнена

---
+++ ex19/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Чтобы узнать является ли число палиндромом,");
+Console.Write("Введите пятизаначное число ");
+int num = int.Parse(Console.ReadLine()??"");
+NumberCheck(num);
+
+///////////////////////////////////////////////////////
+void NumberCheck(int num)
+{
+    int num1 = num / 10000;
+    int num2 = num % 10000 / 1000;
+    int num4 = num % 100 / 10;
+    int num5 = num % 10;
+
+    if(num >= 10000 && num < 100000)
+    {
+        if(num1 == num5 && num2 == num4)
+        Console.WriteLine($"{num} -> да");
+        else Console.WriteLine($"{num} -> нет");
+    }
+    else Console.WriteLine($"Введенное число {num} не является пятизначным");
+}
