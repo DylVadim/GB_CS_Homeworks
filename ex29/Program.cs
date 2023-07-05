abd62c3eb80f232145a71aa61ffb2cc868cf235b
Vadim <dyl.vadim.a@gmail.com>
2023-07-06 00:09:30 +0400

    29 задача выполнено

---
+++ ex29/Program.cs
@@ -0,0 +1,35 @@
+﻿int Prompt(string message)
+{
+    System.Console.Write(message);
+    string readInput = System.Console.ReadLine() ?? "";
+    int result = int.Parse(readInput);
+    return result;
+}
+
+int[] GenerateArray(int Length, int minValue, int maxValue)
+{
+    int[] array = new int[Length];
+    Random random = new Random();
+    for (int i = 0; i < Length; i++)
+    {
+        array[i] = random.Next(minValue, maxValue + 1);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    System.Console.Write("[");
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        System.Console.Write($"{array[i]}, ");
+    }
+    System.Console.Write($"{array[array.Length - 1]}");
+    System.Console.Write("]");
+}
+
+int length = Prompt("Длинна массива: ");
+int min = Prompt("Начальное значение, для диопазона случайного числа: ");
+int max = Prompt("Конечное значение, для диопазона случайного числа: ");
+int[] array = GenerateArray(length, min, max);
+PrintArray(array);
