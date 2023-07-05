b5d1ab0ed9bb300633f30cdec6d0ded30ae86682
Vadim <dyl.vadim.a@gmail.com>
2023-07-06 01:34:38 +0400

    36 задача выполнено

---
+++ ex36/Program.cs
@@ -0,0 +1,43 @@
+﻿int[] array = GenerateArray(-10,10,10);              
+                                                   
+PrintArray(array);                                
+
+int sum = SumElementsOddIndexes(array);
+
+Console.WriteLine($" -> {sum}.");
+
+///////////////////////////////////////////////////////////////////
+
+
+int[] GenerateArray(int min, int max, int length)   
+{                                                   
+    Random random = new Random();                   
+    int[] array = new int [length];                  
+                                                    
+    for (var i = 0; i < array.Length; i++)          
+    {
+        array[i] = random.Next(min, max + 1);       
+    }
+    return array;                                  
+} 
+
+
+void PrintArray(int[] array)                       
+{
+    Console.WriteLine("Сумма элементов, стоящих на нечётных позициях в массивах:");
+    Console.Write(@"[{0}]", string.Join(",", array)); 
+}                                         
+
+
+int SumElementsOddIndexes(int[] array)
+{
+    int sum = 0;
+    for (var i = 0; i < array.Length; i++)
+    {
+        if(i % 2 != 0)
+        {
+            sum = array[i] + sum ;
+        }
+    }
+    return sum;
+}
