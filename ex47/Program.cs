15ae1c2bfed4f0d99df7232a8c7b4ad6892d6a11
Vadim <dyl.vadim.a@gmail.com>
2023-07-06 00:22:37 +0400

    47 задача выполнено

---
+++ ex47/Program.cs
@@ -0,0 +1,32 @@
+﻿double[,] array = CreateRandom2DArray(3, 4);
+
+Print2DArray(array);
+
+
+
+double[,] CreateRandom2DArray(int countOfRows, int countOfColumns)
+{
+    Random random = new Random();
+    double[,] array = new double[countOfRows, countOfColumns];
+
+    for (var i = 0; i < array.GetLength(0); i++)
+    {
+        for (var j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = Math.Round(random.NextDouble() * random.Next(-10, 10), 1);
+        }
+    }
+    return array;
+}
+
+void Print2DArray(double[,] array)
+{
+    for (var i = 0; i < array.GetLength(0); i++)
+    {
+        for (var j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"    {array[i, j]} ");
+        }
+        Console.WriteLine();                      
+    }
+}
